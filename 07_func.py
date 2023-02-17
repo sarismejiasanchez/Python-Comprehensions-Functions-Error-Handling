6b6cbba69700bb96b576d09f2dbc16e1d4c4bb38
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-02-17 00:53:47 -0500

    Funciones
    https://www.w3schools.com/python/python_functions.asp

---
+++ 07_func.py
@@ -0,0 +1,21 @@
+# Funciones
+# https://www.w3schools.com/python/python_functions.asp
+
+print("Hola")
+
+# Definir funcion de impresion
+def my_print(text):
+  print(text * 2)
+
+# Llamado a la funcion
+my_print("This is my text")
+my_print("Hola")
+
+# Funcion de suma
+a = 10
+b = 90
+def sum(a, b):
+  print(a + b)
+
+sum(a, b)
+sum(10, 5)
