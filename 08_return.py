da4831eed1a2410a424043d783a96bef90d55ca4
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-02-17 01:21:21 -0500

    Funciones: return

---
+++ 08_return.py
@@ -0,0 +1,14 @@
+# Funciones: return
+
+def sum_with_range(min, max):
+  print("Range:", min, max)
+  sum = 0
+  for x in range(min, max):
+    sum += x
+  return sum
+
+result = sum_with_range(1, 10)
+print(result)
+
+result_2 = sum_with_range(result, result + 10)
+print(result_2)
