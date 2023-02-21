cfef9ca158fd2e747a32ac0a47c4c779055957fc
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-02-20 23:03:52 -0500

    Reduce

---
+++ 18_reduce.py
@@ -0,0 +1,31 @@
+# Reduce
+# Reduce una lista a un solo valor
+
+import functools
+import random
+
+numbers = [1, 2, 3, 4]
+
+def accum(counter, item):
+  print("counter =>", counter)
+  print("item =>", item)
+  return counter + item
+
+result = functools.reduce(accum, numbers)
+print("reduce function", result)
+
+
+result = functools.reduce(lambda counter, item: counter + item, numbers)
+print("reduce lambda", result)
+
+def mayor(counter, item):
+  if counter <= item:
+    return item
+  else:
+    return counter
+    
+# Mayor valor de una lista de numeros random entre 1 y 10
+numbers = [random.randint(1, 10) for i in range(1, 10)]
+print(numbers)
+result = functools.reduce(mayor, numbers)
+print("Mayor valor =>", result)
