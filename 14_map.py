1af0c7e9472cecabc688e0f30d025c73f99720a1
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-02-20 21:58:04 -0500

    Playgroud: Multiplica todos los elementos por dos

--- 14_map.py
+++ 14_map.py
@@ -28,3 +28,30 @@ result = list(map(lambda x, y: x + y, numbers_1, numbers_2))
 print(numbers_1)
 print(numbers_2)
 print(result)
+
+# Playgroud: Multiplica todos los elementos por dos
+'''
+Para resolver este desafío, tu reto es utilizar la función map de Python y una función lambda para transformar una lista de números. Debes retornar una lista en la que cada número de la lista original sea multiplicado por dos.
+
+La función multiply_numbers recibirá como entrada una lista con números. Finalmente, la función retornará la lista transformada.
+
+Ejemplo 1:
+
+Input: [2, 4, 5, 6, 8]
+Output: [4, 8, 10, 12, 16]
+
+Ejemplo 2:
+
+Input: [1, 1, -2, -3]
+Output: [1, 1, -4, -6]
+'''
+
+def multiply_numbers(numbers):
+  # Escribe tu solución 👇
+  result = list(map(lambda x: x * 2, numbers))
+  return result
+  
+numbers = [1, 2, 3, 4]
+response = multiply_numbers(numbers)
+print("*" * 10)
+print(response)
