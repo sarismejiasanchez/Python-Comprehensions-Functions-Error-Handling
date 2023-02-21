8f4fccc881d3988aa7d598c5fc3c8652a86018d9
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-02-20 22:23:02 -0500

    Playground: Retorna solo palabras de 4 letras y más

--- 17_filter.py
+++ 17_filter.py
@@ -9,3 +9,32 @@ print(new_numbers)
 
 # Filter no modifica la lista original, simplemente crea una nueva lista.
 print(numbers)
+
+# Playground: Retorna solo palabras de 4 letras y más
+'''
+Para resolver este desafío, tu reto es usar la función filter de Python y una función lambda para filtrar una lista de palabras, retornando una lista solo con las que cumplan con la condición de tener 4 o más letras.
+
+La función filter_by_length recibirá como entrada una lista con palabras. Finalmente, la función retornará la lista filtrada.
+
+Ejemplo 1:
+
+Input: ['amor', 'sol', 'piedra', 'día']
+Output: [ 'amor', 'piedra' ]
+
+Ejemplo 2:
+
+Input: ['rosa', 'gol', 'pez', 'día', 'gafas']
+Output: [ 'rosa', 'gafas' ]
+'''
+def filter_by_length(words):
+  # Escribe tu solución 👇
+  new_words = list(filter(lambda x: len(x) >= 4, words))
+  return new_words
+
+words = ['amor', 'sol', 'piedra', 'día']
+response = filter_by_length(words)
+print(response)
+
+words = ['rosa', 'gol', 'pez', 'día', 'gafas']
+response = filter_by_length(words)
+print(response)
