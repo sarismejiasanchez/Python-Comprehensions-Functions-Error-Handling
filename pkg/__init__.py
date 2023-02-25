8ad2163a00b7a427e40551c8efee03857b2c6953
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-02-25 00:36:21 -0500

    Paquetes e __init__.py

---
+++ pkg/__init__.py
@@ -0,0 +1,19 @@
+# Paquetes
+# https://github.com/tiangolo
+# https://github.com/tiangolo/full-stack-fastapi-postgresql
+
+'''
+Antes de la version 3.3 de Python, este archivo se usaba para declarar los paquetes.
+De la 3.3 en adelante se usa solo para inicializar alguna variable o importaciones a paquetes.
+'''
+
+print("Se inicio paquete")
+
+# Cada vez que llame el paquete, inmediatamente se va a crear e inicializar esta variable
+URL = "platzi.com"
+
+'''
+Este archivo tambien puede ser usado para definir un namespace
+'''
+
+import pkg.mod_1, pkg.mod_2
