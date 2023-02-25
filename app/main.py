e7ebf0d8a4fabed42dbe844d83b5de59397df410
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-02-25 00:01:37 -0500

    Módulos como scripts: __name__ y __main__
    Entry point

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 # Importamos el modulo
 import utils
 
-keys, values = utils.get_population()
-print(keys, values)
-
 data = [
   {
     "Country": "Colombia",
@@ -15,6 +12,19 @@ data = [
   }
 ]
 
-country = input("Type Country => ")
-result = utils.population_by_country(data, country)
-print(result)
+def run():
+  keys, values = utils.get_population()
+  print(keys, values)
+  country = input("Type Country => ")
+  result = utils.population_by_country(data, country)
+  print(result)
+
+# Módulos como scripts: __name__ y __main__
+# Entry point
+'''
+Este if le dice al main.py, que si el archivo es ejecutado desde la terminal, ejecute el metodo run, pero si es ejecutado desde otro archivo, el metodo run no se ejecutaria.
+'''
+
+if __name__ == '__main__':
+  run()
+  
