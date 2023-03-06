8dc0459011fdbc453064a434c6d83de78b8034f2
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-03-06 18:33:37 -0500

    Reto: graficando la población mundial

--- app/main.py
+++ app/main.py
@@ -5,8 +5,10 @@ import charts
 
 def run():
   data = read_csv.read_csv('./app/world_population.csv')
-  country = input("Type Country => ")
   
+  '''
+  # Reto: graficando la población de un país
+  country = input("Type Country => ")
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
@@ -15,7 +17,15 @@ def run():
     charts.generate_bar_chart(labels, values)
   
   print(result)
+  '''
 
+  # Reto: graficando la población mundial
+  data = list(filter(lambda item: item["Continent"] == "South America", data))
+  countries = list(map(lambda x: x["Country/Territory"], data))  
+  percentages = list(map(lambda x: x["World Population Percentage"], data))
+
+  charts.generate_pie_chart(countries, percentages)
+  
 # Módulos como scripts: __name__ y __main__
 # Entry point
 '''
